49443f0a03605415608eb214bb1921fbb95c71f2
zainab-ali <zainab.ali.london@gmail.com>
2017-03-24 00:58:39 +0000

    Improve scaladoc

--- src/main/scala/libra/package.scala
+++ src/main/scala/libra/package.scala
@@ -4,12 +4,24 @@ import singleton.ops._
 
 package object libra {
 
+  /** Represents a fraction 
+    * 
+    * @tparam N the numerator
+    * @tparam D the denominator
+    */
   trait Fraction[N <: XInt, D <: XInt]
 
+  /** Represents a single dimension in a HList of dimensions
+    * 
+    * @tparam A the base dimension e.g. Length
+    * @tparam E the fractional exponent the dimension is raised to
+    * 
+    */
   type Term[A, E <: Fraction[_, _]] = shapeless.labelled.FieldType[A, E]
-  /** Represents a quantity with single dimension */
+
+  /** Aliases a quantity with single dimension */
   type QuantityOf[A, T] = Quantity[A, OneOf[T] :: HNil]
 
+  /** Aliases a single dimension */
   type OneOf[A] = Term[A, Fraction[1, 1]]
-
 }
