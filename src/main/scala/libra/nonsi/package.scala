0a2b281934b22e59126a013919efa9d6f0a49d08
Łukasz Drygała <lukasz.drygala@gmail.com>
2017-11-30 22:48:16 +0100

    Add angular units - gradian, turn (#51)

--- src/main/scala/libra/nonsi/package.scala
+++ src/main/scala/libra/nonsi/package.scala
@@ -22,11 +22,15 @@ package object nonsi {
   trait Arcminute extends UnitOfMeasure[Angle]
   trait Arcsecond extends UnitOfMeasure[Angle]
   trait Radian extends UnitOfMeasure[Angle]
+  trait Gradian extends UnitOfMeasure[Angle]
+  trait Turn extends UnitOfMeasure[Angle]
 
   implicit def degreeShow: Show[Degree] = Show[Degree]("degree")
   implicit def arcminuteShow: Show[Arcminute] = Show[Arcminute]("arcminute")
   implicit def arcsecondShow: Show[Arcsecond] = Show[Arcsecond]("arcsecond")
   implicit def radian: Show[Radian] = Show[Radian]("rad")
+  implicit def gradian: Show[Gradian] = Show[Gradian]("gon")
+  implicit def turn: Show[Turn] = Show[Turn]("tr")
 
   implicit def degreeArcminuteConversion[A](
       implicit c: ConvertableTo[A]
@@ -46,7 +50,7 @@ package object nonsi {
   implicit def radianDegreeConversion[A](
       implicit c: ConvertableTo[A]
   ): ConversionFactor[A, Angle, Radian, Degree] =
-    new ConversionFactor(c.fromDouble(180 / pi))
+    new ConversionFactor(c.fromDouble(180.0 / pi))
 
   implicit def radianArcminuteConversion[A](
       implicit c: ConvertableTo[A],
@@ -59,6 +63,58 @@ package object nonsi {
       multiplicative: MultiplicativeSemigroup[A]
   ): ConversionFactor[A, Angle, Radian, Arcsecond] =
     degreeArcsecondConversion.compose(radianDegreeConversion)
+
+  implicit def gradianRadianConversion[A](
+      implicit c: ConvertableTo[A]
+  ): ConversionFactor[A, Angle, Gradian, Radian] =
+    new ConversionFactor(c.fromDouble(pi / 200.0))
+
+  implicit def gradianDegreeConversion[A](
+      implicit c: ConvertableTo[A],
+      multiplicative: MultiplicativeSemigroup[A]
+  ): ConversionFactor[A, Angle, Gradian, Degree] =
+    radianDegreeConversion.compose(gradianRadianConversion)
+
+  implicit def gradianArcminuteConversion[A](
+      implicit c: ConvertableTo[A],
+      multiplicative: MultiplicativeSemigroup[A]
+  ): ConversionFactor[A, Angle, Gradian, Arcminute] =
+    radianArcminuteConversion.compose(gradianRadianConversion)
+
+  implicit def gradianArcsecondConversion[A](
+      implicit c: ConvertableTo[A],
+      multiplicative: MultiplicativeSemigroup[A]
+  ): ConversionFactor[A, Angle, Gradian, Arcsecond] =
+    radianArcsecondConversion.compose(gradianRadianConversion)
+
+  implicit def turnGradianConversion[A](
+      implicit c: ConvertableTo[A]
+  ): ConversionFactor[A, Angle, Turn, Gradian] =
+    new ConversionFactor(c.fromInt(400))
+
+  implicit def turnRadianConversion[A](
+      implicit c: ConvertableTo[A],
+      multiplicative: MultiplicativeSemigroup[A]
+  ): ConversionFactor[A, Angle, Turn, Radian] =
+    gradianRadianConversion.compose(turnGradianConversion)
+
+  implicit def turnDegreeConversion[A](
+      implicit c: ConvertableTo[A],
+      multiplicative: MultiplicativeSemigroup[A]
+  ): ConversionFactor[A, Angle, Turn, Degree] =
+    gradianDegreeConversion.compose(turnGradianConversion)
+
+  implicit def turnArcminuteConversion[A](
+      implicit c: ConvertableTo[A],
+      multiplicative: MultiplicativeSemigroup[A]
+  ): ConversionFactor[A, Angle, Turn, Arcminute] =
+    gradianArcminuteConversion.compose(turnGradianConversion)
+
+  implicit def turnArcsecondConversion[A](
+      implicit c: ConvertableTo[A],
+      multiplicative: MultiplicativeSemigroup[A]
+  ): ConversionFactor[A, Angle, Turn, Arcsecond] =
+    gradianArcsecondConversion.compose(turnGradianConversion)
 
 
 
@@ -73,5 +129,7 @@ package object nonsi {
     def arcminutesPerSecond: AngularVelocityQuantity[A, Arcminute, Second] = Quantity(a)
     def arcsecondsPerSecond: AngularVelocityQuantity[A, Arcsecond, Second] = Quantity(a)
     def radian: QuantityOf[A, Angle, Radian] = Quantity(a)
+    def gradian: QuantityOf[A, Angle, Gradian] = Quantity(a)
+    def turn: QuantityOf[A, Angle, Turn] = Quantity(a)
   }
 }
--- src/test/scala/libra/NonSISpec.scala
+++ src/test/scala/libra/NonSISpec.scala
@@ -8,7 +8,9 @@ import libra.si._
 import org.scalatest._
 
 
-class NonSISpec extends WordSpec {
+class NonSISpec extends WordSpec with Matchers {
+
+  val ε = 1E-9
 
   "degree" should {
     "show" in {
@@ -26,6 +28,14 @@ class NonSISpec extends WordSpec {
     "radian value" in {
       assert(180.0.degree.to[Radian].value === pi)
     }
+
+    "gradian value" in {
+      assert(180.0.degree.to[Gradian].value === 200.0)
+    }
+
+    "turn value" in {
+      assert(720.0.degree.to[Turn].value === 2.0)
+    }
   }
 
   "arcminute" should {
@@ -40,6 +50,18 @@ class NonSISpec extends WordSpec {
     "arcsecond value" in {
       assert(2.0.arcminute.to[Arcsecond].value === 120.0)
     }
+
+    "radian value" in {
+      assert(10800.0.arcminute.to[Radian].value === pi)
+    }
+
+    "gradian value" in {
+      assert(10800.0.arcminute.to[Gradian].value === 200.0)
+    }
+
+    "turn value" in {
+      assert(21600.0.arcminute.to[Turn].value === 1.0)
+    }
   }
 
   "arcsecond" should {
@@ -53,6 +75,18 @@ class NonSISpec extends WordSpec {
 
     "arcminute value" in {
       assert(120.0.arcsecond.to[Arcminute].value === 2.0)
+    }
+
+    "radian value" in {
+      assert(648000.0.arcsecond.to[Radian].value === pi)
+    }
+
+    "gradian value" in {
+      assert(648000.0.arcsecond.to[Gradian].value === 200.0 +- ε)
+    }
+
+    "turn value" in {
+      assert(1296000.0.arcsecond.to[Turn].value === (1.0) +- ε)
     }
   }
 
@@ -104,6 +138,7 @@ class NonSISpec extends WordSpec {
     "radian value" in {
       assert(648000.0.arcsecondsPerSecond.to[Radian].value === pi)
     }
+
   }
 
   "radians" should {
@@ -121,6 +156,67 @@ class NonSISpec extends WordSpec {
 
     "arcsecond value" in {
       assert(pi.radian.to[Arcsecond].value === 648000.0)
+    }
+
+    "gradian value" in {
+      assert(pi.radian.to[Gradian].value === 200.0)
+    }
+
+    "turn value" in {
+      assert(pi.radian.to[Turn].value === 0.5 +- ε)
+    }
+  }
+
+  "gradian" should {
+    "show" in {
+      assert(2.gradian.show === "2 gon [∠]")
+    }
+
+    "degree value" in {
+      assert(200.0.gradian.to[Degree].value === 180.0)
+    }
+
+    "radian value" in {
+      assert(200.0.gradian.to[Radian].value === pi +- ε)
+    }
+
+    "arcminute value" in {
+      assert(200.0.gradian.to[Arcminute].value === 10800.0)
+    }
+
+    "arcsecond value" in {
+      assert(200.0.gradian.to[Arcsecond].value === 648000.0 +- ε)
+    }
+
+    "turn value" in {
+      assert(400.0.gradian.to[Turn].value === 1)
+    }
+
+  }
+
+  "turn" should {
+    "show" in {
+      assert(2.turn.show === "2 tr [∠]")
+    }
+
+    "gradian value" in {
+      assert(1.0.turn.to[Gradian].value === 400.0)
+    }
+
+    "degree value" in {
+      assert(1.0.turn.to[Degree].value === 360.0)
+    }
+
+    "radian value" in {
+      assert(1.0.turn.to[Radian].value === (2 * pi) +- ε)
+    }
+
+    "arcminute value" in {
+      assert(1.0.turn.to[Arcminute].value === 21600.0)
+    }
+
+    "arcsecond value" in {
+      assert(1.0.turn.to[Arcsecond].value === 1296000.0 +- ε)
     }
   }
 }
